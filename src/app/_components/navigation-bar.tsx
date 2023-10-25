39499b030984cbcb43740fc2e84fadf114f9d408
CottonCandyZ <zztgqk@gmail.com>
2023-10-25 12:01:19 +0800

    Make parent component simple.

--- src/app/_components/navigation-bar.tsx
+++ src/app/_components/navigation-bar.tsx
@@ -3,39 +3,34 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { CONFIG } from "~/config/base";
 
-
-export default function NavigationBar() {
+export default function NavigationBar({ className }: { className?: string }) {
   const pathname = usePathname();
   const router_content = CONFIG.card_router;
   return (
-    <nav>
+    <nav className={className}>
       <ul className="flex justify-center space-x-4">
-        {router_content.map((item, index) => {
-          return pathname == item.href ? (
-            <ListItem key={index} href={item.href} current={true}>
-              <item.icon className="text-[1em] mr-1"/>
-              {item.name}
-            </ListItem>
-          ) : (
-            <ListItem key={index} href={item.href}>
-              <item.icon className="text-[0px] mr-0 
-        group-hover:text-[1em] group-hover:mr-1 group-hover:transition-all group-hover:duration-150
-        group-hover:animate-backshake"/>
-              {item.name}
-            </ListItem>
-          );
-        })}
+        {router_content.map((item, index) => (
+          <ListItem
+            key={index}
+            content={item.name}
+            href={item.href}
+            Icon={item.icon}
+            current={pathname == item.href}
+          />
+        ))}
       </ul>
     </nav>
   );
 }
 
 function ListItem({
-  children,
+  content,
+  Icon,
   href,
   current = false,
 }: {
-  children: React.ReactNode;
+  content: string;
+  Icon: React.FC<{ className?: string }>;
   href: string;
   current?: boolean;
 }) {
@@ -43,22 +38,26 @@ function ListItem({
     <li className="inline-block">
       <Link
         href={href}
-        className="relative z-0 flex items-center 
-    pl-3 pr-3 before:absolute before:bottom-0 before:left-0 before:right-0 before:-z-10 
-    before:h-full before:rounded-xl before:bg-red-200 pointer-events-none"
+        className="pointer-events-none relative z-0 flex 
+    items-center pl-3 pr-3 before:absolute before:bottom-0 before:left-0 before:right-0 
+    before:-z-10 before:h-full before:rounded-xl before:bg-red-200"
       >
-        {children}
+        <Icon className="mr-1 text-[1em]"></Icon>
+        {content}
       </Link>
     </li>
   ) : (
     <li className="inline-block">
       <Link
         href={href}
-        className="relative z-0 flex items-center 
-        before:duration-150 before:absolute before:bottom-0 before:left-0 before:right-0 before:-z-10 before:h-0.5 before:rounded-xl 
-      before:bg-red-200 before:transition-all hover:pl-3 hover:pr-3 hover:before:h-full group"
+        className="group relative z-0 flex 
+        items-center before:absolute before:bottom-0 before:left-0 before:right-0 before:-z-10 before:h-0.5 before:rounded-xl 
+      before:bg-red-200 before:transition-all before:duration-150 hover:pl-3 hover:pr-3 hover:before:h-full"
       >
-        {children}
+        <Icon className="mr-0 text-[0px] 
+        group-hover:mr-1 group-hover:animate-backshake group-hover:text-[1em] group-hover:transition-all
+        group-hover:duration-150" />
+        {content}
       </Link>
     </li>
   );
