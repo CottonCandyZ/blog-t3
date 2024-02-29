d8e333b255348c5cb3623b2eba717fe7a109b285
CottonCandyZ <zztgqk@gmail.com>
2024-02-29 19:48:41 +0800

    Back to next build in Google font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,14 @@
 import "~/styles/globals.scss";
+import "~/styles/markdown.scss";
+import { Noto_Sans_SC } from 'next/font/google'
 import { headers } from "next/headers";
 import { TRPCReactProvider } from "~/trpc/react";
 import Footer from "~/components/footer";
 import MainWrapper from "~/components/main-wrapper";
+
+const noto_sans_sc = Noto_Sans_SC({
+  subsets: ['latin']
+});
 
 const themeNumber = Math.floor(Math.random() * 8) + 1;
 export default function RootLayout({
@@ -12,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`antialiased theme-${themeNumber}`}>
+      <body className={`antialiased theme-${themeNumber} ${noto_sans_sc.className}}`}>
         <TRPCReactProvider headers={headers()}>
           <MainWrapper>
             {children}
