669ef105b59cdcfae658ea7d3c96fa34783220b8
CottonCandyZ <zztgqk@gmail.com>
2024-03-07 21:12:58 +0800

    Add: Streaming loading

--- src/components/posts/mdx-component.tsx
+++ src/components/posts/mdx-component.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+'use client'
+import { useMemo } from "react";
 import { getMDXComponent } from "mdx-bundler/client";
 import type { MDXContentProps } from "mdx-bundler/dist/types";
 import NextImage from "~/components/posts/content/Image";
@@ -9,34 +10,33 @@ import ListItem from "~/components/posts/content/lists/ListItem";
 import CodeBlock from "~/components/posts/content/Codeblock";
 import MDXLink from "~/components/posts/content/link";
 
-
-export default function PostContent({ code }: { code: string }) {
+export default function MDXComponent({ code }: { code: string }) {
   const components = {
-    h1: tagRenderer('h1'),
-    h2: tagRenderer('h2'),
-    h3: tagRenderer('h3'),
-    h4: tagRenderer('h4'),
-    h5: tagRenderer('h5'),
-    h6: tagRenderer('h6'),
-    p: tagRenderer('p'),
-    blockquote: tagRenderer('blockquote'),
-    table: tagRenderer('table'),
-    thead: tagRenderer('thead'),
-    tbody: tagRenderer('tbody'),
-    tr: tagRenderer('tr'),
-    th: tagRenderer('th'),
-    td: tagRenderer('td'),
-    em: tagRenderer('em'),
-    strong: tagRenderer('strong'),
-    del: tagRenderer('del'),
+    h1: tagRenderer("h1"),
+    h2: tagRenderer("h2"),
+    h3: tagRenderer("h3"),
+    h4: tagRenderer("h4"),
+    h5: tagRenderer("h5"),
+    h6: tagRenderer("h6"),
+    p: tagRenderer("p"),
+    blockquote: tagRenderer("blockquote"),
+    table: tagRenderer("table"),
+    thead: tagRenderer("thead"),
+    tbody: tagRenderer("tbody"),
+    tr: tagRenderer("tr"),
+    th: tagRenderer("th"),
+    td: tagRenderer("td"),
+    em: tagRenderer("em"),
+    strong: tagRenderer("strong"),
+    del: tagRenderer("del"),
     img: NextImage,
     ul: UnorderedList,
     ol: OrderedList,
     li: ListItem,
-    hr: tagRenderer('hr'),
+    hr: tagRenderer("hr"),
     code: CodeBlock,
     a: MDXLink,
   } as unknown as MDXContentProps["components"];
-  const Component = React.useMemo(() => getMDXComponent(code), [code]);
+  const Component = useMemo(() => getMDXComponent(code), [code]);
   return <Component components={components} />;
 }
---
+++ src/components/posts/post-content.tsx
@@ -0,0 +1,25 @@
+import { getPostContent } from "~/lib/posts";
+import MDXComponent from "~/components/posts/mdx-component";
+import TableOfContents from "~/components/posts/table-of-contents";
+
+export default async function PostContent({ slug }: { slug: string }) {
+  const { code, frontmatter } = await getPostContent(decodeURIComponent(slug));
+  return (
+    <div className="mx-auto flex max-w-6xl flex-row justify-center gap-10 px-5 py-10 md:px-10 lg:justify-normal">
+      <article className="markdown-body relative w-full max-w-2xl">
+        <h2 className="anchor invisible absolute -top-6" id="Introduction">
+          {frontmatter.title}
+        </h2>
+        <MDXComponent code={code} />;
+      </article>
+      <aside className="sticky top-28 hidden max-h-[calc(-112px+100vh)] basis-1/3 overflow-auto p-2 lg:block lg:gap-4">
+        <nav className="flex flex-col gap-2">
+          <h2 className="text-2xl font-medium tracking-wider text-primary">
+            目录
+          </h2>
+          <TableOfContents />
+        </nav>
+      </aside>
+    </div>
+  );
+}
