456ea4caf93a9d41af2e75676318da9398afe65e
CottonCandyZ <zztgqk@gmail.com>
2024-03-13 16:42:48 +0800

    Add friend link page.

--- src/app/link/page.tsx
+++ src/app/link/page.tsx
@@ -1,7 +1,13 @@
+import LinkCard from "~/components/link/link-card";
+import { CONFIG } from "~/config";
+
 export default function page() {
   return (
-    <div className="">
-      施工中...最后会做成卡片的形式
-    </div>
+    <main className="mx-auto mt-2 flex min-h-[calc(-300px+100dvh)] 
+    max-w-6xl flex-row flex-wrap content-start px-5 pb-20 gap-5 md:px-10">
+      {CONFIG.friend_link.map((info, index) => (
+        <LinkCard key={index} props={info} />
+      ))}
+    </main>
   );
 }
