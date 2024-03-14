6348f3e3c278d78422778d68639e09a2f26b590b
CottonCandyZ <zztgqk@gmail.com>
2024-03-14 12:37:15 +0800

    Add search element to Tags section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,12 +16,14 @@ export default async function Page() {
     <div className="relative grid auto-rows-max grid-cols-[2fr_1fr] gap-10 ">
       <TagsProvider>
         <section className="col-span-full row-start-1 h-min md:sticky md:top-24 md:col-start-2 md:block">
-          <h2 className="text-xl font-medium tracking-widest text-primary">
-            Tags (Click to filter)
-          </h2>
-          <div className="mt-3 h-min">
-            <Tags uniqueTags={uniqueTags} oTags={oTags} />
-          </div>
+          <search>
+            <h2 className="text-xl font-medium tracking-widest text-primary">
+              Tags (Click to filter)
+            </h2>
+            <div className="mt-3 h-min">
+              <Tags uniqueTags={uniqueTags} oTags={oTags} />
+            </div>
+          </search>
         </section>
         <section className="col-span-full row-start-2 md:col-start-1 md:col-end-1 md:row-start-1">
           <h2 className="text-xl font-medium tracking-widest text-primary">
