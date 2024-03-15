67d673895c1a030a9d771198c235cf921df2c5aa
CottonCandyZ <zztgqk@gmail.com>
2024-03-15 21:03:16 +0800

    Change ul and markdown body style.

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -164,13 +164,10 @@ export const Dot: React.FC<SVGProps<SVGSVGElement>> = (props) => {
       xmlns="http://www.w3.org/2000/svg"
       width="1em"
       height="1em"
-      viewBox="0 0 16 16"
+      viewBox="0 0 24 24"
       {...props}
     >
-      <path
-        fill="currentColor"
-        d="M8 9.5a1.5 1.5 0 1 0 0-3a1.5 1.5 0 0 0 0 3"
-      ></path>
+      <path fill="currentColor" d="M12 16a4 4 0 1 1 0-8a4 4 0 0 1 0 8Z"></path>
     </svg>
   );
 };
