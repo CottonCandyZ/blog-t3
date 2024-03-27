bd6844b37526e0b0fc91da047a87a2c2d072fd5f
CottonCandyZ <zztgqk@gmail.com>
2024-03-28 00:12:44 +0800

    Change theme change logic

--- src/components/theme-wrapper.tsx
+++ src/components/theme-wrapper.tsx
@@ -1,8 +1,8 @@
-'use client'
-import { useRef, type PropsWithChildren } from "react";
+import { type PropsWithChildren } from "react";
+export const dynamic = 'force-dynamic';
 const ThemeWrapper: React.FC<PropsWithChildren> = ({ children }) => {
-  const themeNumber = useRef(Math.floor(Math.random() * 8) + 1);
-  return <body className={`theme-${themeNumber.current}`}>{children}</body>;
+  const themNumber = Math.floor(Math.random() * 8) + 1;
+  return <body className={`theme-${themNumber}`}>{children}</body>;
 };
 
 export default ThemeWrapper;
