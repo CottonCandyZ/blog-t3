da8da5eaf2e62680248177adca1a524042ecdf30
CottonCandyZ <zztgqk@gmail.com>
2024-04-19 22:07:50 +0800

    Improve: change header opacity

--- src/components/header/header-with-wrapper.tsx
+++ src/components/header/header-with-wrapper.tsx
@@ -3,7 +3,7 @@ import Header from "~/components/header/header";
 const HeaderWithWrapper = () => {
   return (
     <div className="sticky top-0 z-10 mx-auto max-w-6xl px-2 md:px-10 ">
-      <div className="rounded-b-2xl bg-primary-bg p-4 shadow-cxs">
+      <div className="rounded-b-2xl bg-primary-bg/95 p-4 shadow-cxs">
         <Header />
       </div>
     </div>
