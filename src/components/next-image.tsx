d37fdbbdf6663744bd066076517c886a38db0958
CottonCandyZ <zztgqk@gmail.com>
2024-08-12 22:51:55 +0800

    [fix]: next image center

--- src/components/next-image.tsx
+++ src/components/next-image.tsx
@@ -1,7 +1,7 @@
 'use client'
 import Image from 'next/image'
 import { useState } from 'react'
-import { ImageProps } from '~/server/tools/image';
+import { ImageProps } from '~/server/tools/image'
 
 export default function NextImage({
   src,
@@ -13,7 +13,7 @@ export default function NextImage({
 }: ImageProps & { alt: string; className?: string }) {
   const [load, setLoad] = useState(false)
   return (
-    <div className="mdx-img relative w-full overflow-hidden">
+    <div className="mdx-img relative flex w-full justify-center overflow-hidden">
       {!load && <div className="absolute inset-0 size-full scale-150 blur-2xl" style={blurCss} />}
       <Image
         className={className}
