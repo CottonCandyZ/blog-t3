ee7fc2bbffb2fa4c44bb110785d47112c169bf11
CottonCandyZ <06-arid.zilch@icloud.com>
2025-01-22 01:20:36 +0800

    fix(ui): 调整整体宽度后的一些细节调整
    - 头像宽度
    - content 宽度

--- src/components/profile/profile-card.tsx
+++ src/components/profile/profile-card.tsx
@@ -9,7 +9,7 @@ function ProfileCard() {
     <div className="flex flex-col items-center gap-2 p-3 ">
       <Link
         href="/about"
-        className="group relative max-w-60 overflow-hidden rounded-xl active:scale-95"
+        className="group relative max-w-60 overflow-hidden rounded-xl active:scale-95 md:max-w-none"
       >
         <Image src={profileImage} placeholder="blur" alt="Picture of the author" />
         <div className="absolute inset-0 flex items-center justify-center bg-primary-light/40 opacity-0 transition-all group-hover:opacity-100">
